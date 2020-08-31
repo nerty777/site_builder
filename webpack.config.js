4afe9e884de09cfca3319ba5e20a55214f7a1a17
Ivan <nerty777@gmail.com>
2020-08-31 10:46:45 +0300

    structure app

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,13 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
-  })]
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
